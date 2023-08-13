a5343b8267fb0280b43e612bd90b8acc6afe4db3
hanbintan2003 <thb182003@gmail.com>
2023-08-12 17:32:52 -0700

    ok

---
+++ app/(auth)/sign-up/layout.tsx
@@ -0,0 +1,12 @@
+export const metadata = {
+    title: 'Threads',
+    description: 'A place to share your thoughts',
+}
+
+export default function RootLayout ({ 
+    children
+} : { 
+    children: React.ReactNode
+}){
+    
+}
